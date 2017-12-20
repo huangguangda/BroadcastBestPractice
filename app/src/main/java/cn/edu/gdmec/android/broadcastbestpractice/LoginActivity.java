cfd664fdbe793388c5ca0b3eef402918da8a7fe0
huangguangda <2397923107@qq.com>
2017-12-20 23:59:16 +0800

    205

--- app/src/main/java/cn/edu/gdmec/android/broadcastbestpractice/LoginActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/broadcastbestpractice/LoginActivity.java
@@ -1,8 +1,12 @@
 package cn.edu.gdmec.android.broadcastbestpractice;
 
+import android.widget.EditText;
+
 /**
  * Created by Jack on 2017/12/20.
  */
 
-public class LoginActivity {
+public class LoginActivity extends BaseActivity{
+    private EditText accountEdit;
+
 }
