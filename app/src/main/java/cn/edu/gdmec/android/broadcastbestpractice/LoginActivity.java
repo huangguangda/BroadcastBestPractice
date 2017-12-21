13c0acf79b104858d3a2d6028cd77dab67be9711
huangguangda <2397923107@qq.com>
2017-12-21 16:01:29 +0800

    205111

--- app/src/main/java/cn/edu/gdmec/android/broadcastbestpractice/LoginActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/broadcastbestpractice/LoginActivity.java
@@ -1,9 +1,11 @@
 package cn.edu.gdmec.android.broadcastbestpractice;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by Jack on 2017/12/20.
@@ -28,8 +30,16 @@ public class LoginActivity extends BaseActivity{
             public void onClick(View view) {
                 String account = accountEdit.getText ().toString ();
                 String password = passwordEdit.getText ().toString ();
+
+                if (account.equals ( "admin" )&&password.equals ( "123456" )){
+                    Intent intent = new Intent ( LoginActivity.this,
+                            MainActivity.class );
+                    startActivity ( intent );
+                    finish ();
+                }else{
+                    Toast.makeText ( LoginActivity.this, "account or password", Toast.LENGTH_SHORT ).show ();
+                }
             }
         } );
     }
-
 }
