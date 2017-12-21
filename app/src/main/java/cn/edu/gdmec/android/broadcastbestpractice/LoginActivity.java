dd0c39e744cc23a6e87fb578686c67bdfe481ba1
huangguangda <2397923107@qq.com>
2017-12-21 15:52:41 +0800

    205

--- app/src/main/java/cn/edu/gdmec/android/broadcastbestpractice/LoginActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/broadcastbestpractice/LoginActivity.java
@@ -1,5 +1,8 @@
 package cn.edu.gdmec.android.broadcastbestpractice;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 /**
@@ -9,5 +12,24 @@ import android.widget.EditText;
 public class LoginActivity extends BaseActivity{
     private EditText accountEdit;
     private EditText passwordEdit;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.login );
+
+        accountEdit = (EditText) findViewById ( R.id.account );
+        passwordEdit = (EditText) findViewById ( R.id.password );
+
+        login = (Button) findViewById ( R.id.login );
+        login.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View view) {
+                String account = accountEdit.getText ().toString ();
+                String password = passwordEdit.getText ().toString ();
+            }
+        } );
+    }
 
 }
